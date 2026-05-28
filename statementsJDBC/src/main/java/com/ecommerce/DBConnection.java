2b3da7a330a7081b482728a2759265d772fa4f3f
Akho10 <APhungula@sars.gov.za>
2026-05-28 15:06:45 +0200

    Added jdbc setup, mysql products can show

---
+++ statementsJDBC/src/main/java/com/ecommerce/DBConnection.java
@@ -0,0 +1,31 @@
+package com.ecommerce;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+	private Connection connection;
+	
+	public DBConnection(String dbUrl, String user, String pwd) throws ClassNotFoundException, SQLException{
+		
+        Class.forName("com.mysql.jdbc.Driver");
+        this.connection = DriverManager.getConnection(dbUrl, user, pwd);
+
+
+	}
+
+	public Connection getConnection() {
+		return this.connection;
+	}
+
+	public void closeConnection() throws SQLException{
+		if(this.connection != null) {
+			this.connection.close();
+		}
+	}
+	
+	
+	
+}
